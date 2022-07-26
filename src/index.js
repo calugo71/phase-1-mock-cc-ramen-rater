e6a559d7424cb1bca068da1f99f371b78b314bf0
Carlos Lugo <calugo71@gmail.com>
2022-07-26 11:39:19 -0400

    first try. issue first establishing fetch

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 // write your code here
+//Major user error when first creating the fetch. killed too much time on it. 
+//used fetch source with '../ramens' at the end
+function getRamen() {
+    fetch('http://localhost:3000/ramens')
+    .then(res => res.json())
+    .then(data => console.log(data))}
+getRamen();
+
+function renderRamenMenu() {
+    let menu=getElementById('ramen-menu');
+    img.src = 'http://localhost:3000/ramens';
+    let img = document.createElement('img')
+    menu.add.img
+}
